1e6b6b1c41a67f55bbb3b6134795d9641a7e3dd8
saratarek <saratarek224@hotmail.com>
2019-05-15 01:11:45 +0200

    user register and login

--- users/models.py
+++ users/models.py
@@ -3,10 +3,18 @@ from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 # Create your models here.
 
-class CustomUser(User):
-    pass
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     phone = models.CharField(
         max_length=11,
         validators=[RegexValidator('(01)[0-9]{9}',
                                    message="phone number is not valid")])
-    profilePics = models.ImageField(upload_to='profilePics', blank=True)
+    profilePic = models.ImageField(upload_to='profilePic', blank=True)
+
+
+def __str__(self):
+  return self.user.username
+
+
+
+
